3006149c2f537baf71b3e47377dfcf46c3d820ea
Riley Seaburg <riley@rds.marketing>
2020-05-06 05:09:48 -0400

    add game logic

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import Pokedex from './Pokedex'
+import Pokegame from './Pokegame'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    <Pokedex
-      id={4}
-      name="Charmander"
-      type="fire"
-      exp={62}
+    <Pokegame
     />
     </div>
   );
--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Pokecard from './Pokecard'
+import './Pokedex.css'
 
 export class Pokedex extends Component {
     static defaultProps = {
@@ -15,9 +16,17 @@ export class Pokedex extends Component {
           ]
     }
     render() {
+        let title;
+        if (this.props.isWinner) {
+            title = <h1 className="Pokedex-winner">Winning Hand</h1>
+        } else {
+          title = <h1 className="Pokedex-loser">Losing Hand</h1>
+        }
         return (
             <div className="Pokedex" >
-                <h1>Pokedex</h1>
+            {title}
+                <h4>Total Expierence: {this.props.exp}</h4>
+                <div className="Pokedex-cards">
                 {this.props.pokemon.map((p) => (
                     <Pokecard 
                     id={p.id}
@@ -26,6 +35,8 @@ export class Pokedex extends Component {
                     exp={p.base_experience}
                     />
                 ))}
+                </div>
+                
             </div>
         )
     }
